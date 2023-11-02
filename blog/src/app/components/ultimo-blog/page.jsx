bce82758a72f3d90a917d0ef3401a01b12d443d5
r3verse <sebastian.ohberg@proton.me>
2023-11-01 21:45:36 -0300

    Header [0.1.0]

---
+++ blog/src/app/components/ultimo-blog/page.jsx
@@ -0,0 +1,5 @@
+export default function Blog () {
+    return(
+            <h1>Soy un blog</h1>
+    )
+}
---
+++ blog/src/app/page.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/header/page"
+import Blog from "./components/ultimo-blog/page"
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Blog />
+    </>
+  )
+}
