09591b1c5f71dcb5e8f58094485b18ada714a547
r3verse <sebastian.ohberg@proton.me>
2023-12-02 00:15:54 -0300

    Corrigiendo error en build [1.1.0]

--- blog/src/app/Bloglist/page.jsx
+++ blog/src/app/Bloglist/page.jsx
@@ -1,13 +1,15 @@
 import React from "react"
 import { allBlogs } from "contentlayer/generated"
-import Card from "./Card"
+// import Card from "./Card"
 
 export default function BlogList() {
-    const sortBlogs = (allBlogs)
+    // const sortBlogs = (allBlogs)
     return(
         <section className="w-full mt-5 px-5 items-center justify-center flex flex-col ">
             <h2 className="w-full inline-block font-bold capitalize px-10">Lista de blogs</h2>
-            <Card blogs={sortBlogs}/>
+            <h3>Estamos trabajando en solucionar el problema</h3>
+            {//<Card blogs={sortBlogs}/>
+            }
         </section>
     )
 }
